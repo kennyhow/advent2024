24ae4c3248085a9a7598bad320162352573ee080
kennyhow <kennethyonghw@gmail.com>
2024-12-05 21:02:18 +0800

    Finish day 5

--- day5/sol.js
+++ day5/sol.js
@@ -18,6 +18,55 @@ const isValid = (rules, input) => {
     return result;
 };
 
+const buildMatrix = (rules, input) => {
+    let matrix = new Array(100).fill(0).map(() => new Array(100).fill(-1));
+    rules.forEach(([a, b]) => {
+        matrix[a][b] = true;
+        matrix[b][a] = false;
+    });
+
+    let change = true;
+    while(change) {
+        change = false;
+        input.forEach((a) => {
+            input.forEach((b) => {
+                input.forEach((c) => {
+                    if (matrix[a][b] == true && matrix[b][c] == true && matrix[a][c] == -1) {
+                        matrix[a][c] = true;
+                        matrix[c][a] = false;
+                        change = true;
+                    }
+                    if (matrix[a][b] == false && matrix[b][c] == false && matrix[a][c] == -1) {
+                        matrix[a][c] = false;
+                        matrix[c][a] = true;
+                        change = true;
+                    }
+                })
+            })
+        });
+    }
+
+    input.forEach((a) => {
+        input.forEach((b) => {
+            console.log(`a: ${a}, b: ${b}, result: ${matrix[a][b] == -1 ? "invalid" : (matrix[a][b] == true ? " (a, b) " : " (b, a) ")}`);
+        });
+    });
+    console.log();
+    return matrix;
+};
+
+const fixInput = (rules, input) => {
+    let elements = new Set(input);
+    rules = rules.filter(([a, b]) => {
+        return elements.has(a) && elements.has(b);
+    });
+    console.log(`rules is ${JSON.stringify(rules)} and input is ${input}`);
+
+    let matrix = buildMatrix(rules, input.slice());
+    input.sort((a, b) => matrix[a][b] == true ? -1 : (matrix[a][b] == false ? 1 : 0));
+    return input;
+};
+
 fs.readFile('input.txt', 'utf-8', (_, data) => {
     data = data.trim().split('\n');
     let rules = [];
@@ -49,4 +98,17 @@ fs.readFile('input.txt', 'utf-8', (_, data) => {
     });
     console.log(valid);
     console.log(valid.reduce((acc, total) => acc + total));
+
+    let invalid = inputs.map((input) => {
+        let result = isValid(rules, input);
+        if (result) {
+            return 0;
+        }
+        else {
+            let fixed = fixInput(rules, input);
+            return fixed[Math.floor(fixed.length / 2)];
+        }
+    });
+    console.log(invalid);
+    console.log(invalid.reduce((acc, current) => acc + current));
 });
