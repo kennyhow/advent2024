a50876a44377f3cc174f59713e84bd56451300ad
kennyhow <kennethyonghw@gmail.com>
2024-12-05 18:27:07 +0800

    Finish day 1 :D

--- day1/sol.js
+++ day1/sol.js
@@ -5,7 +5,6 @@ fs.readFile("input.txt", "utf-8", (_, data) => {
 
   const parse_into_tuple = (x) => {
     x = x.trim().split(/\s+/);
-    console.log(x.map(Number));
     return x.map(Number);
   };
 
@@ -23,6 +22,18 @@ fs.readFile("input.txt", "utf-8", (_, data) => {
   let ans = 0;
   left_col.forEach((value, index) => {
     ans += Math.abs(value - right_col[index]);
+  });
+  console.log(ans);
+
+  right_freq = new Map();
+  right_col.forEach((element) => {
+    right_freq.set(element, right_freq.has(element) ? right_freq.get(element) + 1 : 1);
+  });
+  console.log(right_freq);
+
+  ans = 0;
+  left_col.forEach((element) => {
+    ans += right_freq.has(element) ? element * right_freq.get(element) : 0;
   });
   console.log(ans);
 });
